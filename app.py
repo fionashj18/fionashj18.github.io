4af1ece9dab3c38aece4626d26e22f98434645e2
fionashj <shixfiona15@gmail.com>
2026-01-24 12:39:05 -0500

    new features

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
